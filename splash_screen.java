cc284f2c652674e85fc9a7ea22a240f9aa2ce98d
shlphw07 <125403866+shlphw07@users.noreply.github.com>
2023-02-26 09:51:57 +0530

    Create splash_screen.java

---
+++ splash_screen.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Intent iLogin= new Intent(SplashScreen.this,MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(iLogin);
+                finish();
+
+            }
+        },500);
+
+
+    }
+}
