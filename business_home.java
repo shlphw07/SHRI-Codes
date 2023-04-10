2731787ad428ca7a015026cd795475be3bca62db
TheHEXG <113372996+TheHEXG@users.noreply.github.com>
2023-04-10 14:07:16 +0530

    Create business_home.java

---
+++ business_home.java
@@ -0,0 +1,14 @@
+package com.harshbhardwaj.shriuli100;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
